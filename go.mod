cc3c0d50042a3be92796fd6e05ff71a2f5b399d4
shubhpro <shubhpro@192.168.1.4>
2022-06-27 18:58:07 +0530

    go code

---
+++ go.mod
@@ -0,0 +1,3 @@
+module goprepbytes
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Is the starting point of your application
+func main() {
+
+	fmt.Println("Hello Mom !!!!")
+
+}
